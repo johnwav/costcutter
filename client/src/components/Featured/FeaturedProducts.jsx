2318969f8b01671e1d956db5dc505459f99fe341
johnwav <102820657+johnwav@users.noreply.github.com>
2023-04-30 00:52:39 +0100

    Feat: Featured product

--- client/src/components/Featured/FeaturedProducts.jsx
+++ client/src/components/Featured/FeaturedProducts.jsx
@@ -4,55 +4,23 @@ import "./FeaturedProducts.scss";
 import axios from "axios";
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/904350/pexels-photo-904350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/1374910/pexels-photo-1374910.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Long Sleeve T-shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
 
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2090784/pexels-photo-2090784.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Coat",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1039518/pexels-photo-1039518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Coat",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Coat",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
 
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await axios.get(
-          import.meta.env.VITE_API_URL + "/products",
+        const res = await axios.get(
+          import.meta.env.VITE_API_URL + "/products?populate=*",
           {
-            Authorization: "bearer" + import.meta.env.VITE_API_TOKEN,
+            headers: {
+              Authorization: "bearer" + import.meta.env.VITE_API_TOKEN,
+            },
           }
         );
-        console.log(data);
+        setData(res.data.data);
+        console.log(res.data)
       } catch (error) {
         console.log(error);
       }
